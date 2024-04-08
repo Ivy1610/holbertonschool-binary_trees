053b82b137decd65f3dc83d493314e4dfb264280
Ivy1610 <8682@holbertonstudent.com>
2024-04-08 16:17:44 +0200

    modifier le code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,11 +11,15 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new_node_child_left;
 
 	if (parent == NULL)
-		new_node_child_left = binary_tree_node(parent, value);
-
-	return (NULL);
-	if (new_node_child_left == NULL)
+	{
 		return (NULL);
+	}
+	new_node_child_left = binary_tree_node(parent, value);
+
+	if (new_node_child_left == NULL)
+	{
+		return (NULL);
+	}
 	if (parent->left != NULL)
 	{
 		new_node_child_left->left = parent->left;
